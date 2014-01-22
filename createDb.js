dbae32d7667bf014d614fe43203bdd075c0e16f7
dananskyi <Dmitry.Anansky@dataart.com>
2014-01-22 18:40:43 +0200

    Add createDb.js fot test users

--- createDb.js
+++ createDb.js
@@ -1,15 +1,46 @@
 var mongoose = require('./libs/mongoose');
-var User     = require('./models/user').User;
+//mongoose.set('debug', true);
+var async    = require('async');
 
-var user = new User({
-    username: "Testerr",
-    password: "secret"
+
+async.series([
+    open,
+    dropDatabase,
+    requireModels,
+    createUsers
+], function(err, results) {
+    console.log(arguments);
+    mongoose.disconnect();
+    process.exit(err ? 255 : 0);
 });
 
-user.save(function(err, user, affected) {
-    if (err) throw err;
-    User.findOne({username: "Testerr"}, function(err, tester) {
-        if (err) throw err;
-        console.log(tester);
-    });
-});
+function open(callback) {
+    mongoose.connection.on('open', callback);
+}
+
+function dropDatabase(callback) {
+    var db = mongoose.connection.db;
+    db.dropDatabase(callback);
+    console.log('DropDatabase: Ok');
+}
+
+function requireModels(callback) {
+    require('./models/user').User;
+
+    async.each(Object.keys(mongoose.models), function(modelName, callback) {
+        mongoose.models[modelName].ensureIndexes(callback);
+    }, callback);
+}
+
+function createUsers(callback) {
+    var users = [
+        {username: 'testOne', password: 'test1'},
+        {username: 'testTwo', password: 'test2'},
+        {username: 'testThree', password: 'test3'}
+    ];
+    async.each(users, function(userData, callback) {
+        var user = new mongoose.models.User(userData);
+        user.save(callback);
+    }, callback);
+}
+
