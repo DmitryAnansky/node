cadd12c73c2ff93619e6222f99a9ce4673cc7733
DimaAnanskyi <starkandtesla@gmail.com>
2014-02-24 00:20:40 +0200

    Fixed logout issue

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ module.exports = function(app) {
 
     app.get('/login', require('./login').get);
     app.post('/login', require('./login').post);
-    app.post('/logout', require('./logout').post);
 
+    app.post('/logout', require('./logout').post);
 
     app.get('/chat', checkAuth, require('./chat').get);
 }
