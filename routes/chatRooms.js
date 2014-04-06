4d002e042fee2e0b8cd0e8d18d3de83236a45649
DimaAnanskyi <starkandtesla@gmail.com>
2014-04-07 00:13:49 +0300

    rooms

--- routes/chatRooms.js
+++ routes/chatRooms.js
@@ -1,7 +1,16 @@
+var HttpError = require('../error').HttpError;
+
 exports.get = function(request, response) {
     response.render('chatRooms');
 }
 
-exports.post = function(request, response) {
+exports.post = function(request, response, next) {
+    var roomName = request.body.roomName;
 
+    if(!roomName) {
+            return next(new HttpError(403,"No room name fined"));
+    }
+
+    console.log(roomName);
+    response.send({});
 }
